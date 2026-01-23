ac969b819cafb07c34d0c59a2327e6ebe967d7d4
PhantomNota09 <srayala5@asu.edu>
2026-01-22 20:36:03 -0700

    added signup pages

---
+++ RegisterAPP/Model/LoginResponse.swift
@@ -0,0 +1,15 @@
+//
+//  LoginResponse.swift
+//  RegisterAPP
+//
+//  Created by Surya Rayala on 1/21/26.
+//
+
+struct LoginResponse {
+    var loggedInUserId: String?
+    var loggedInUserName: String?
+    var loggedInUserEmail: String?
+    var token: String?
+    var profileImage: String?
+}
+
